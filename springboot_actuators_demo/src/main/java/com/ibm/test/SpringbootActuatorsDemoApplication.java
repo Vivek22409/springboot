734359dc429d2a69b599b538d54783555527f4b3
Vivek <vivekkumar02@vivek3148418-01.Nagarro.local>
2020-04-07 13:08:55 +0530

    Initial Commit

---
+++ springboot_actuators_demo/src/main/java/com/ibm/test/SpringbootActuatorsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ibm.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActuatorsDemoApplication.class, args);
+	}
+
+}
