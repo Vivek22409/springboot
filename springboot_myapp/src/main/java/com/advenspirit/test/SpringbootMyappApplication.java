96fa46746dd2073ea20a5729e70b782c0838abb1
Vivek <vivekkumar02@vivek3148418-01.Nagarro.local>
2020-04-08 15:17:10 +0530

    Initial commit

---
+++ springboot_myapp/src/main/java/com/advenspirit/test/SpringbootMyappApplication.java
@@ -0,0 +1,13 @@
+package com.advenspirit.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMyappApplication.class, args);
+	}
+
+}
