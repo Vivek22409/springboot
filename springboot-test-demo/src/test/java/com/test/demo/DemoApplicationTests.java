6aeab33bb2c4d0e4029c018650f0156d09cea9ad
Vivek <vivekkumar02@host.docker.internal>
2020-05-25 15:27:27 +0530

    Commit test Project

--- springboot-test-demo/src/test/java/com/test/demo/DemoApplicationTests.java
+++ springboot-test-demo/src/test/java/com/test/demo/DemoApplicationTests.java
@@ -2,14 +2,19 @@ package com.test.demo;
 
 
 
-
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
 import com.test.demo.controller.Controller;
 import org.springframework.util.Assert;
 
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
 	
@@ -17,8 +22,14 @@ public class DemoApplicationTests {
 	private Controller con;
 
 	@Test
+	@DirtiesContext
 	public void testControllerBean() {
 		Assert.notNull(con, "Object not null");
+	}
+	
+	@Test
+	public void testControolerBeanMethod() {
+		Assert.notNull(con.greeting("TEST_NAME", Mockito.mock(Model.class)),"Object not null");
 	}
 
 }
